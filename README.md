f55c887014185a3d890b45a66998f654d60235dd
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-05 14:59:35 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,13 @@
+URL: https://www.dedhamsavings.com/whats-new/
+
+    Spider name: dedhamsavings
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 9.26
+
+Number of articles: 30
---
+++ dedhamsavings/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'dedhamsavings'
+SPIDER_MODULES = ['dedhamsavings.spiders']
+NEWSPIDER_MODULE = 'dedhamsavings.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'dedhamsavings.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
